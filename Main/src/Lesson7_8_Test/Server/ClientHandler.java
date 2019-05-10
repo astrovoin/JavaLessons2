0da1b120b1db3bb917d146cf2d2a9edc1d843e29
Voinduha <voinduha@list.ru>
2019-05-10 21:18:17 +0800

    Домашнее задание к 8 уроку Установлен таймаут в 120 секунд Если нет авторизации, то клиента отключает

--- Main/src/Lesson7_8_Test/Server/ClientHandler.java
+++ Main/src/Lesson7_8_Test/Server/ClientHandler.java
@@ -1,5 +1,7 @@
 package Lesson7_8_Test.Server;
 
+import org.omg.CORBA.TIMEOUT;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -11,9 +13,8 @@ public class ClientHandler {
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
-
     private String name;
-
+    private static boolean taskComplete = false;
     public String getName() {
         return name;
     }
@@ -27,6 +28,7 @@ public class ClientHandler {
             this.name = "";
             new Thread(() -> {
                 try {
+
                     authentication();
                     readMessages();
 
@@ -39,14 +41,55 @@ public class ClientHandler {
         } catch (IOException e) {
             throw new RuntimeException("Проблемы при создании обработчика клиента");
         }
+
     }
 
+    public class TimeOut implements Runnable {
+        public void run() {
+            try {
 
+                Thread.sleep(120000);  //120 секунд
+                if(taskComplete ==false) {
+                    System.out.println("Timed Out");
+
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+                else {
+                    return;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
     public void authentication() throws IOException {
         while (true) {
+            // Создается новый поток с ожиданием в 120 секунд
+            // Если клиент не авторизируется за это время, соединение закрывается.
+            TimeOut timeOut = new TimeOut();
+            Thread  timeOutThread = new Thread(timeOut);
+            timeOutThread.start();
             String str = in.readUTF();
-            if (str.startsWith("/auth")) {
+
+                if (str.startsWith("/auth")) {
                 String[] parts = str.split("\\s");
                 String nick = myServer.getAuthService().getNickByLoginPass(parts[1], parts[2]);
                 if (nick != null) {
@@ -55,6 +98,8 @@ public class ClientHandler {
                         name = nick;
                         myServer.broadcastMsg(name + " зашел в чат");
                         myServer.subscribe(this);
+                        // если авторизация успешна выставляется флаг
+                        taskComplete = true;
                         return;
                     } else {
                         sendMsg("Учетная запись уже используется");
